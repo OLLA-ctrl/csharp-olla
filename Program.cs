48ec056e26f2ab749a62baa0df756a629ed1dce0
GROUPE8 <ollaatete3@gmail.com>
2026-03-18 14:34:44 +0100

    C'est mon premier programme C#git add .

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nom = "olla";
+        int age = 21;
+
+        Console.WriteLine("Bonjour !");
+        Console.WriteLine($"Je m'appelle {nom} et j'ai {age} ans.");
+
+        string[] amis = { "kwizera", "Chahrazad", "ella" };
+
+        Console.WriteLine("Mes amis :");
+        Console.WriteLine(amis[0]);
+        Console.WriteLine(amis[1]);
+        Console.WriteLine(amis[2]);
+    }
+}
